bfbc58acb8d9fc664fafb0c9023633fb923036ca
Yerbol <yerbolsend@gmail.com>
2023-03-14 12:49:11 +0600

    tag ready

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Main\IndexController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,10 +13,35 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return 'ddddddd';
+Route::name('main')->group(function() {
+    Route::get('/', IndexController::class);
 });
+
+
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
+   Route::group(['namespace'=>'Main'], function(){
+       Route::get('/','IndexController');
+    });
+    Route::group(['namespace'=>'Category', 'prefix'=>'category'], function(){
+        Route::get('/','IndexController')->name('admin.category.index');
+        Route::get('/create','CreateController')->name('admin.category.create');
+        Route::post('/','StoreController')->name('admin.category.store');
+        Route::get('/{category}','ShowController')->name('admin.category.show');
+        Route::get('/{category}/edit','EditController')->name('admin.category.edit');
+        Route::patch('/{category}','UpdateController')->name('admin.category.update');
+        Route::delete('/{category}','DeleteController')->name('admin.category.delete');
+    });
+    Route::group(['namespace'=>'Tag', 'prefix'=>'tags'], function(){
+        Route::get('/','IndexController')->name('admin.tag.index');
+        Route::get('/create','CreateController')->name('admin.tag.create');
+        Route::post('/','StoreController')->name('admin.tag.store');
+        Route::get('/{tag}','ShowController')->name('admin.tag.show');
+        Route::get('/{tag}/edit','EditController')->name('admin.tag.edit');
+        Route::patch('/{tag}','UpdateController')->name('admin.tag.update');
+        Route::delete('/{tag}','DeleteController')->name('admin.tag.delete');
+    });
+});
+
 
 Auth::routes();
 
